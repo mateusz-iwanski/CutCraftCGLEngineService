140ae3031a0a797603194193f57aa5caf5a4c006
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-18 20:25:14 +0200

    Add ICalculator interface and necessary using directives
    - Added `using` directives for common .NET namespaces.
    - Introduced `CutCraftEngineWebSocketCGLService` namespace.
    - Defined `ICalculator` interface within the new namespace.
    - Added summary comment for `ICalculator` interface.
    - Declared `Execute` method in `ICalculator` interface.

---
+++ ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService
+{
+    /// <summary>
+    /// Interface for all calculators included in the service (CutGlib, TonCut, etc)
+    /// </summary>
+    public interface ICalculator
+    {
+        public string Execute();
+    }
+}
