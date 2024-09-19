5b3e0a1455204e674fbf127903b778d7ce1b0867
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-19 23:01:21 +0200

    Add CGLCalculator class implementing ICalculator
    Introduced the CGLCalculator class in the CutCraftEngineWebSocketCGLService.CGLCalculator namespace. This class implements the ICalculator interface and uses the CutEngine class to execute commands. Added necessary using directives and implemented the Execute method to set up and execute commands with CutEngine.

---
+++ CGLCalculator/CGLCalculator.cs
@@ -0,0 +1,24 @@
+﻿using CutCraftEngineData.DataInput;
+using CutGLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.CGLCalculator
+{
+    public class CGLCalculator : ICalculator
+    {
+        private readonly CutEngine _cutEngine;
+        
+        public CGLCalculator() => 
+            _cutEngine = new CutEngine();
+
+        public string Execute(Command command)
+        {
+            new CGLCutEngineSetup(command, _cutEngine);
+            return _cutEngine.Execute();
+        }
+    }
+}
