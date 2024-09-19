3ce9ce37be52815dcde3f95eb51bcf009ba7585a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-19 23:02:18 +0200

    Add CGLCutEngineSetup class for CutEngine initialization
    Introduce CGLCutEngineSetup class in CutCraftEngineWebSocketCGLService.CGLCalculator namespace to set up CutEngine using Command data. Import necessary namespaces. Initialize cutEngine in the constructor, processing stock and material data, and configure properties such as width, height, count, ID, waste, piece width, piece length, piece quantity, rotation, and identifier.

---
+++ CGLCalculator/CGLCutEngineSetup.cs
@@ -0,0 +1,52 @@
+﻿using CutCraftEngineData.DataInput;
+using CutCraftEngineWebSocketCGLService.DataInput;
+using CutGLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.CGLCalculator
+{
+    internal class CGLCutEngineSetup
+    {
+
+        public readonly CutEngine cutEngine;
+
+        public CGLCutEngineSetup(Command command, CutEngine cutEngine)
+        {
+            var stockList = command.InputWrapper.input.stock;
+
+            foreach (var stock in stockList)
+            {
+                var materials = command.InputWrapper.input.materials;
+
+                foreach (var material in materials)
+                {
+                    cutEngine.SetProperties(command, material.deviceId, material.kerf);
+
+                    var cutEngineInputData = new CutEngine2DStockItemInputData(command, material, StockPriority.normal);
+
+                    cutEngine.AddStock(
+                        cutEngineInputData.aWidth,
+                        cutEngineInputData.aHeight,
+                        cutEngineInputData.aCount,
+                        cutEngineInputData.aID,
+                        cutEngineInputData.aWaste
+                        );
+
+                    cutEngine.AddPart(
+                        cutEngineInputData.Piece.width,
+                        cutEngineInputData.Piece.length,
+                        cutEngineInputData.Piece.quantity,
+                        cutEngineInputData.Piece.Rotated(),
+                        cutEngineInputData.Piece.identifier
+                        );
+                }
+            }
+
+            this.cutEngine = cutEngine;
+        }
+    }
+}
