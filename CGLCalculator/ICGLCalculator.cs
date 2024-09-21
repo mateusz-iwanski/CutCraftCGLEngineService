8c7effc7d41b70c4e97c97c494feab8119413f1c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-21 13:10:25 +0200

    Add ICGLCalculator interface with GetCutEngine method
    Introduce a new interface `ICGLCalculator` in the
    `CutCraftEngineWebSocketCGLService.CGLCalculator` namespace.
    This interface includes a method `GetCutEngine()` that returns
    an instance of `CutEngine`. Added necessary `using` directives
    for `CutGLib`, `System`, `System.Collections.Generic`,
    `System.Linq`, `System.Text`, and `System.Threading.Tasks`.

---
+++ CGLCalculator/ICGLCalculator.cs
@@ -0,0 +1,14 @@
+﻿using CutGLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.CGLCalculator
+{
+    public interface ICGLCalculator
+    {
+        public CutEngine GetCutEngine();
+    }
+}
