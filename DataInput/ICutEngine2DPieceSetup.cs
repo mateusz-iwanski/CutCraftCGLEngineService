b6057a48b33073f418337bb29147b57ff7b91524
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-24 10:04:30 +0200

    Add ICutEngine2DPieceSetup interface in new namespace
    A new namespace `CutCraftEngineWebSocketCGLService.DataInput` has been introduced. Within this namespace, the `ICutEngine2DPieceSetup` interface has been added, which includes properties (`aCount`, `aHeight`, `aID`, `aRotatable`, `aWidth`) and methods (`Rotatable()`, `SizeReal()`, `Surplus(int dimension)`) related to a 2D piece setup in a cutting engine context.

---
+++ DataInput/ICutEngine2DPieceSetup.cs
@@ -0,0 +1,15 @@
+﻿namespace CutCraftEngineWebSocketCGLService.DataInput
+{
+    public interface ICutEngine2DPieceSetup
+    {
+        int aCount { get; }
+        double aHeight { get; }
+        string aID { get; }
+        bool aRotatable { get; }
+        double aWidth { get; }
+
+        bool Rotatable();
+        dynamic SizeReal();
+        int Surplus(int dimension);
+    }
+}
