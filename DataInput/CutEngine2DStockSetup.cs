0e3969dce4a73a64bfb0e4904aaf05bab52898ee
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-24 10:04:03 +0200

    Add CutEngine2DStockSetup class for stock data unification
    Introduce CutEngine2DStockSetup class in CutCraftEngineWebSocketCGLService.DataInput namespace. This class implements ICutEngine2DStockSetup interface to unify stock data before adding it to the CutGLib engine. Key properties include id, aWidth, aHeight, aCount, aID, and aWaste. The constructor initializes these properties using an IStockItem instance. Added private method getStockPriority to determine stock priority based on input string.

---
+++ DataInput/CutEngine2DStockSetup.cs
@@ -0,0 +1,66 @@
+﻿using CutCraftEngineData.DataInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataInput
+{
+    /// <summary>
+    /// Unify the Stock data from the input data before adding it to the CutGLib engine.
+    /// </summary>
+    public class CutEngine2DStockSetup : ICutEngine2DStockSetup
+    {
+        private readonly IStockItem _stock;
+
+        public int id { get; set; }
+
+        /// <summary>
+        /// Width (length) of the StockItem.
+        /// </summary>
+        public double aWidth { get; set; }
+
+        /// <summary>
+        /// Height of the StockItem.
+        /// </summary>
+        public double aHeight { get; set; }
+
+        /// <summary>
+        /// Specifies quantity of the stock items.
+        /// </summary>
+        public int aCount { get; set; }
+
+        /// <summary>
+        /// The user defined identifier of the StockItem. It must be Code128 barcode compatible.
+        /// </summary>
+        public string aID { get; set; }
+
+        /// <summary>
+        /// If aWaste is true then such stock will be used first before any actual 
+        /// stocks that have aWaste as false.
+        /// </summary>
+        public bool aWaste { get; set; }
+
+        public CutEngine2DStockSetup(IStockItem stock)
+        {
+            _stock = stock;
+
+            id = stock.id;
+            aWidth = stock.length;
+            aHeight = stock.width;
+            aCount = stock.quantity;
+            aID = stock.identifier;
+            aWaste = Convert.ToBoolean(
+                (int)getStockPriority(stock.priority)
+                );
+        }
+
+        /// <summary>
+        /// If priority is normal then such stock will be used first before any actual stocks that have low priority. 
+        /// </summary>
+        private StockPriority getStockPriority(string priority) => priority == "normal" ? StockPriority.high : StockPriority.normal;
+    }
+
+}
