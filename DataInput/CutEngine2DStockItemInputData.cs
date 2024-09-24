80fd30c13ad5cd6b335aba1a48b3bd53bc2017e0
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-24 10:03:41 +0200

    Refactor CutEngine2DStockItemInputData class
    Replaced `Piece` and `StockItem` lists with `ICutEngine2DPieceSetup` and `ICutEngine2DStockSetup` lists. Removed `priority` parameter from constructor and added XML documentation. Introduced `UnifyPieces` and `UnifyStocks` methods for data conversion. Added XML documentation for new methods.

--- DataInput/CutEngine2DStockItemInputData.cs
+++ DataInput/CutEngine2DStockItemInputData.cs
@@ -23,13 +23,25 @@ namespace CutCraftEngineWebSocketCGLService.DataInput
     /// </remarks>
     public class CutEngine2DStockItemInputData
     {
-        public readonly List<IPiece> Piece;        
-        public readonly List<IStockItem> StockItem;
+        public readonly List<ICutEngine2DPieceSetup> Piece;        
+        public readonly List<ICutEngine2DStockSetup> Stock;
 
-        public CutEngine2DStockItemInputData(Command command, IMaterial material, StockPriority prioriy = StockPriority.normal)
+        /// <summary>
+        /// Constructor find stock and pieces with specific material.
+        /// 
+        /// Pieces are unify to CutGLib standard.
+        /// </summary>
+        /// <param name="command">Command with DataInput</param>
+        /// <param name="material">Material to find pieces and stocks</param>
+        /// <param name="prioriy"></param>
+        /// <exception cref="Exception"></exception>
+        public CutEngine2DStockItemInputData(Command command, IMaterial material)
         {
-            StockItem = command.Input.stock.Where(s => s.materialId == material.id).Cast<IStockItem>().ToList() ?? throw new Exception($"Stock with Material ID: {material.id} does not exist in the list of stocks to use.");
-            Piece = command.Input.pieces.Where(p => p.materialId == material.id).Cast<IPiece>().ToList() ?? throw new Exception($"Piece with Material ID: {material.id} does not exist in the list of pieces to use.");
+            var stocksFromCutGLib = command.Input.stock.Where(s => s.materialId == material.id).Cast<IStockItem>().ToList() ?? throw new Exception($"Stock with Material ID: {material.id} does not exist in the list of stocks to use.");            
+            var pieceFromCutGLib = command.Input.pieces.Where(p => p.materialId == material.id).Cast<IPiece>().ToList() ?? throw new Exception($"Piece with Material ID: {material.id} does not exist in the list of pieces to use.");
+
+            Piece = UnifyPieces(pieceFromCutGLib);
+            Stock = UnifyStocks(stocksFromCutGLib);
 
             if (!Is2D(material)) throw new Exception("The material type (kind) has to be 2D");
             if (material.standardStockItems.Count == 0 && command.AllowOverstock == true) throw new Exception("StandardStockItem is mandatory when the allowOverstock is enabled.");
@@ -44,5 +56,40 @@ namespace CutCraftEngineWebSocketCGLService.DataInput
         ///         2D materials like glass, panels, fabric, aluminum, cardboard, etc.
         /// </summary>
         public bool Is2D(IMaterial material) => material.kind == "2d" ? true : false;
+
+        /// <summary>
+        /// Unify the Piece data from the input data before adding it to the CutGLib engine.
+        /// </summary>
+        /// <param name="pieces">List of pieces from DataInput</param>
+        /// <returns>Unify pices in list</returns>
+        private List<ICutEngine2DPieceSetup> UnifyPieces(List<IPiece> pieces)
+        {
+            var result = new List<ICutEngine2DPieceSetup>();
+
+            foreach (var piece in pieces)
+            {
+                result.Add(new CutEngine2DPieceSetup(piece));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Unify the Piece data from the input data before adding it to the CutGLib engine.
+        /// </summary>
+        /// <param name="stocks">List of stocks from DataInput</param>
+        /// <returns>Unify stocks in list</returns>
+        private List<ICutEngine2DStockSetup> UnifyStocks(List<IStockItem> stocks)
+        {
+            var result = new List<ICutEngine2DStockSetup>();
+
+            foreach (var stock in stocks)
+            {
+                result.Add(new CutEngine2DStockSetup(stock));
+            }
+
+            return result;
+        }
+
     }
 }
