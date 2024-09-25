ee25622de247540625c4abce0f999db24dc40e0e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:48:14 +0200

    Add CGLProcessDataOutput class with data processing methods
    Introduce the CGLProcessDataOutput class in the CutCraftEngineWebSocketCGLService.DataOutput namespace. This class includes private properties for cut engine, command, and data outputs. A constructor initializes command and data outputs, while methods are added to set the cut engine, build data output, retrieve default units, and return the version of the cut engine. Several namespaces are imported to support these functionalities. Commented-out sections indicate potential future features for building statistics and cuttings.

---
+++ DataOutput/CGLProcessDataOutput.cs
@@ -0,0 +1,100 @@
+﻿using CutCraftEngineData.DataInput;
+using CutCraftEngineData.DataOutput;
+using CutCraftEngineData.DefaultUnits;
+using CutCraftEngineWebSocketCGLService.CGLCalculator;
+using CutCraftEngineWebSocketCGLService.DataInput;
+using CutGLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    public class CGLProcessDataOutput
+    {
+        private CutEngine _cutEngine { get; set; }
+        private readonly Command _command;
+        private readonly DataOutputs _dataOutputs;
+
+        public CGLProcessDataOutput(Command command)
+        {
+            _command = command;
+
+            _dataOutputs = new DataOutputs();
+
+            _dataOutputs.cuttings = new List<Cutting>();
+
+            return;
+        }
+
+        public void SetCutEngine(CutEngineInitializer calculator)
+        {
+            if (calculator.IsExecuted == false)
+                throw new InvalidOperationException("Calculator must be executed before processing data.");
+
+            _cutEngine = calculator.GetCutEngine();
+        }
+
+        public DataOutputs BuildDataOutput()
+        {
+            return new DataOutputs()
+            {
+                version = BuildVersion(),
+                defaultUnits = BuildDefaultUnits(),
+            };
+        }
+
+        /// <summary>
+        /// Get the DefaultUnits.
+        /// 
+        /// CutGlib does not have DefaultUnits in the output, must be retrieved from the input command.
+        /// </summary>
+        /// <returns></returns>
+        private DefaultUnits BuildDefaultUnits()
+        {
+            DefaultUnitsInput defaultUnitsInput = _command.Input.defaultUnits;
+
+            return new DefaultUnits(
+                _angle: defaultUnitsInput.angle,
+                _length: defaultUnitsInput.length,
+                _field: defaultUnitsInput.field
+                );
+        }
+
+        //private Statistics BuildStatistics()
+        //{
+        //    return new Statistics()
+        //    {
+        //        _1d = new Statistics1d(),
+        //        _2d = new Statistics2d(
+        //            field: _cutEngine.UsedStockCount,
+        //            usedField: ,
+        //            wasteField:,
+        //            unusedField:,
+        //            cutCount:,
+        //            cutsLength:
+        //            )
+        //    };
+        //}
+
+        //private void BuildCuttings(ICutEngine2DStockSetup stock, )
+        //{
+        //    _dataOutputs.cuttings.Add(
+        //        new Cutting()
+        //        {
+        //            stockItemId = stock.id,
+        //            quantity = _cutEngine.UsedStockCount,
+        //            pieces = new List<Piece>()
+        //        }
+        //    );
+        //}
+
+        public string BuildVersion()
+        {
+            return _cutEngine.Version;
+        }
+
+    }
+}
