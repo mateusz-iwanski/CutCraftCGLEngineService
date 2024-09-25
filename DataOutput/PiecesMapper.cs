fa891be4bb38ab206c066463552dc7a7560928e8
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:53:27 +0200

    Add PiecesMapper class for mapping CGLPart to CuttingPiece
    Introduced a new class `PiecesMapper` in the `CutCraftEngineWebSocketCGLService.DataOutput` namespace to map `CGLPart` objects to `CuttingPiece` objects. The class includes:
    - A private field `_parts` to store the list of `CGLPart` objects.
    - A constructor to initialize the `_parts` field.
    - A `Map` method to convert the list of `CGLPart` objects to a list of `CuttingPiece` objects, setting properties like `pieceId`, `x`, `y`, `rotated`, and `mirrored`.
    
    Added necessary `using` directives for required namespaces.

---
+++ DataOutput/PiecesMapper.cs
@@ -0,0 +1,42 @@
+﻿using CutCraftEngineData.DataOutput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// Mapping list of CutGLib CGLPart to list of DataOutput CuttingPiece
+    /// </summary>
+    public class PiecesMapper
+    {
+        List<CGLPart> _parts;
+
+        public PiecesMapper(List<CGLPart> parts)
+        {
+            _parts = parts;
+            return;
+        }
+
+        public List<CuttingPiece> Map()
+        {
+            var pieces = new List<CuttingPiece>();
+
+            foreach (var part in _parts)
+            {
+                pieces.Add(new CuttingPiece()
+                    {
+                        pieceId = part.Id,
+                        x = part.X,
+                        y = part.Y,
+                        rotated = part.Rotated,
+                        mirrored = false
+                    });
+            }
+
+            return pieces;
+        }
+    }
+}
