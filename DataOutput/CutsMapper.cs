6babf703987c8efd07c5506fbc78d828dbb2f64f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:49:05 +0200

    Add CutsMapper class for mapping CGLCut to Cut objects
    Added a new CutsMapper class in the CutCraftEngineWebSocketCGLService.DataOutput namespace.
    The CutsMapper class maps a list of CGLCut objects to a list of Cut objects.
    Includes a private readonly field _cuts, a constructor, and a Map method.
    The Map method converts each CGLCut object to a Cut object, mapping properties X1, Y1, X2, and Y2 to startX, startY, endX, and endY, respectively.

---
+++ DataOutput/CutsMapper.cs
@@ -0,0 +1,40 @@
+﻿using CutCraftEngineData.DataOutput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// Mapping list of CutGLib CGLCut to list of DataOutput Cut
+    /// </summary>
+    public class CutsMapper 
+    {
+        private readonly List<CGLCut> _cuts;
+
+        public CutsMapper(List<CGLCut> cuts)
+        {
+            _cuts = cuts;
+            return;
+        }
+
+        public List<Cut> Map()
+        {            
+            var cuts = new List<Cut>();
+            foreach (var cut in _cuts)
+            {
+                cuts.Add(new Cut()
+                {
+                    startX = cut.X1,
+                    startY = cut.Y1,
+                    endX = cut.X2,
+                    endY = cut.Y2
+                });
+            }
+            
+            return cuts;
+        }
+    }
+}
