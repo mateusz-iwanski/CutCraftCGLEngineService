68f49aeb3e883d5a2cad2a87293ca812a3c6aac0
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:46:55 +0200

    Add CGLLayout class to represent CutGLib layout results
    Introduced a new class `CGLLayout` in the `CutCraftEngineWebSocketCGLService.DataOutput` namespace. This class includes properties for layout number, start sheet, list of sheets, count of sheets, and list of parts. Added necessary `using` directives for `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`.

---
+++ DataOutput/CGLLayout.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// Layout from CutGLib results
+    /// </summary>
+    public class CGLLayout
+    {
+        /// <summary>
+        /// The layout number
+        /// </summary>
+        public int Layout { get; init; }
+
+        /// <summary>
+        /// The sheet on which the first parts is placed
+        /// </summary>
+        public CGLSheet StartSheet { get; init; }
+
+        /// <summary>
+        /// List of sheets in the layout
+        /// </summary>
+        public List<CGLSheet> Sheets { get; init; }
+
+        /// <summary>
+        /// The number of sheets placed in the stack when cutting (cut in package)
+        /// </summary>
+        public int CountOfSheets { get; init; }
+
+        /// <summary>
+        /// List of parts placed on the sheet/s
+        /// </summary>
+        public List<CGLPart> Parts { get; init; } 
+
+    }
+}
