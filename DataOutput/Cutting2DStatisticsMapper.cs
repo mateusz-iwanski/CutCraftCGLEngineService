33f73a25937962db1b9642dc5406a3769fc8558f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:49:19 +0200

    Add Cutting2DStatisticsMapper for data mapping
    Introduce Cutting2DStatisticsMapper class in Cutting2DStatisticsMapper.cs
    within the CutCraftEngineWebSocketCGLService.DataOutput namespace.
    This class maps data from CGLStatistics2D to CuttingStatistics2d.
    Includes necessary using directives, a constructor to initialize
    CGLStatistics2D, and a Map method to perform the data conversion.

---
+++ DataOutput/Cutting2DStatisticsMapper.cs
@@ -0,0 +1,33 @@
+﻿using CutCraftEngineData.DataOutput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    public class Cutting2DStatisticsMapper
+    {
+        CGLStatistics2D _statics2D;
+
+        public Cutting2DStatisticsMapper(CGLStatistics2D statics2D)
+        {
+            _statics2D = statics2D;
+            return;
+        }
+
+        public CuttingStatistics2d Map()
+        {
+            return new CuttingStatistics2d()
+            {
+                field = _statics2D.field,
+                usedField = _statics2D.usedField,
+                wasteField = _statics2D.wasteField,
+                unusedField = _statics2D.unusedField,
+                cutCount = _statics2D.cutCount,
+                cutsLength = _statics2D.cutsLength
+            };
+        }
+    }
+}
