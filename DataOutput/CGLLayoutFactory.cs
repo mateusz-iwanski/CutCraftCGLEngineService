3f8480d0cef71d1936d9f31604188bbd67c6518d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:47:13 +0200

    Add CGLLayoutFactory to build layouts from CutGLib results
    Introduce CGLLayoutFactory class in CutCraftEngineWebSocketCGLService.DataOutput namespace.
    - Add necessary using directives.
    - Implement ICGLDataOutputFactory<CGLLayout> interface.
    - Define private fields for CutEngine, CGLLayout, CGLSheet, and CGLPart.
    - Provide constructor to initialize fields and call Build method.
    - Implement Build method to construct CGLLayout objects and handle errors.
    - Add Get method to return list of CGLLayout objects.

---
+++ DataOutput/CGLLayoutFactory.cs
@@ -0,0 +1,70 @@
+﻿using CutGLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// Build layout from CutGLib results
+    /// </summary>
+    public class CGLLayoutFactory : ICGLDataOutputFactory<CGLLayout>
+    {
+        private readonly CutEngine _cutEngine;
+
+        private List<CGLLayout> _layout;
+        private List<CGLSheet> _sheets;
+        private List<CGLPart> _parts;
+
+        public CGLLayoutFactory(CutEngine cutEngine, List<CGLSheet> sheets, List<CGLPart> parts)
+        {
+            _cutEngine = cutEngine;
+
+            _layout = new List<CGLLayout>();
+            _sheets = sheets;
+            _parts = parts;
+
+            Build();
+        }
+
+        /// <summary>
+        /// Add layouts to the list of CGLLayout
+        /// </summary>
+        private void Build()
+        {
+            int sheetIndex, StockCount, iLayout;            
+
+            // Iterate by each layout and output information about each layout,
+            // such as number and length of used stocks and part indices cut from the stocks
+            for (iLayout = 0; iLayout < _cutEngine.LayoutCount; iLayout++)
+            {
+                _cutEngine.GetLayoutInfo(iLayout, out sheetIndex, out StockCount);
+                // sheetIndex is global index of the first sheet used in the layout iLayout
+                // StockCount is quantity of sheets of the same size as sheetIndex used for this layout
+                if (StockCount > 0)
+                {
+                    var startSheet = _sheets.FirstOrDefault(s => s.Sheet == sheetIndex) ?? 
+                        throw new Exceptions.CustomException($"Can't find Sheet index ({sheetIndex})");
+
+                    var parts = _parts.Where(p => p.Sheet.Sheet == sheetIndex).ToList();
+
+                    _layout.Add(new CGLLayout
+                    {
+                        Layout = iLayout,
+                        StartSheet = startSheet,
+                        CountOfSheets = StockCount,
+                        Parts = parts
+                    });
+                }
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Get layouts list
+        /// </summary>
+        public List<CGLLayout> Get() => _layout;
+    }
+}
