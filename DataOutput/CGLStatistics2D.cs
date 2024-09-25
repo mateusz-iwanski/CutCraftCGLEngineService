7a749c3f9a952f363d4536864e2f968111646c13
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:48:53 +0200

    Add CGLStatistics2D class for trim cut statistics
    Introduced a new class `CGLStatistics2D` in the `CutCraftEngineWebSocketCGLService.DataOutput` namespace, inheriting from `Statistics2d`. This class includes properties for `CutTrimCount`, `CutsTrimLength`, `Layout`, and `Sheet`. Added necessary namespace imports to support the new class.

---
+++ DataOutput/CGLStatistics2D.cs
@@ -0,0 +1,42 @@
+﻿using CutCraftEngineData.DataInput;
+using CutCraftEngineData.DataOutput;
+using CutCraftEngineWebSocketCGLService.CGLCalculator;
+using CutGLib;
+using NLog.Layouts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// 2D statistics from CutGLib results.
+    /// </summary>
+    public class CGLStatistics2D : Statistics2d
+    {
+        /// <summary>
+        /// The number of trim cuts.
+        /// </summary>
+        public int CutTrimCount { get; init; }
+
+        /// <summary>
+        /// The total length of all trim cuts
+        /// </summary>
+        public double CutsTrimLength { get; init; }
+
+        /// <summary>
+        /// The layout in which the statistics are calculated
+        /// </summary>
+        public CGLLayout Layout { get; init; }
+
+        /// <summary>
+        /// Sheet in layout
+        /// 
+        /// Layout has only one sheet with specific width and height
+        /// </summary>
+        public CGLSheet Sheet { get; init; }
+    }
+}
