a85d45cbd3ca0237f23b535fa414e9cea707494f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:47:45 +0200

    Add CGLPart class to represent parts from CutGLib results
    Introduce CGLPart class in CutCraftEngineWebSocketCGLService.DataOutput namespace.
    This class includes properties like Id, PartId, Sheet, X, Y, Width, Height, and Rotated,
    all of which are read-only and initialized via object initializers.
    Add necessary using directives for common namespaces.

---
+++ DataOutput/CGLPart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// Piece/part from CutGLib results 
+    /// </summary>
+    public class CGLPart
+    {
+        /// <summary>
+        /// Unique id of part from external data input system
+        /// </summary>
+        public int Id { get; init; }
+
+        /// <summary>
+        /// Part id assigned by CutGLib
+        /// </summary>
+        public int PartId { get; init; }
+
+        /// <summary>
+        /// Sheet on which the part is placed
+        /// </summary>
+        public CGLSheet Sheet { get; init; }
+
+        /// <summary>
+        /// X position of the piece.
+        /// </summary>
+        public double X { get; init; }
+
+        /// <summary>
+        /// Y position of the piece.
+        /// </summary>
+        public double Y { get; init; }
+
+        /// <summary>
+        /// Width of the piece.
+        /// </summary>
+        public double Width { get; init; }
+
+        /// <summary>
+        /// Height of the piece.
+        /// </summary>
+        public double Height { get; init; }
+
+        /// <summary>
+        /// If piece is rotated
+        /// </summary>
+        public bool Rotated { get; init; }
+}
+}
