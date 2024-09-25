7c12b3907d39a2c84c61cf478ccfb3992553405e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:48:28 +0200

    Add CGLSheet class to represent sheet data in CutGLib results
    The changes introduce a new class `CGLSheet` within the
    `CutCraftEngineWebSocketCGLService.DataOutput` namespace. This class
    represents a sheet from CutGLib results and includes properties for
    `Sheet` (an integer representing the sheet number), `Width` (a double
    representing the width of the sheet), and `Height` (a double
    representing the height of the sheet). Additionally, several namespaces
    are imported to support the new class.

---
+++ DataOutput/CGLSheet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// Sheet from CutGLib results
+    /// </summary>
+    public class CGLSheet
+    {
+        /// <summary>
+        /// Sheet number
+        /// </summary>
+        public int Sheet { get; init; }
+
+        /// <summary>
+        /// Width 
+        /// </summary>
+        public double Width { get; init; }
+
+        /// <summary>
+        /// Height
+        /// </summary>
+        public double Height { get; init; }
+    }
+}
