86ddcd053278460414ad4d3d5d9907088e125b7f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:48:40 +0200

    Add CGLSheetFactory to build CGLSheet objects
    Introduced CGLSheetFactory class in CutCraftEngineWebSocketCGLService.DataOutput namespace to build CGLSheet objects from a CutEngine instance. Implemented ICGLDataOutputFactory<CGLSheet> interface. Added private fields for CutEngine reference and CGLSheet list. Constructor initializes CutEngine and calls Build method. Build method processes layouts and stocks to populate CGLSheet list. Get method returns the list of CGLSheet objects. Added necessary using directives.

---
+++ DataOutput/CGLSheetFactory.cs
@@ -0,0 +1,71 @@
+﻿using CutGLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// Build CGLSheet objects from CutEngine
+    /// </summary>
+    internal class CGLSheetFactory : ICGLDataOutputFactory<CGLSheet>
+    {
+        private readonly CutEngine _cutEngine;
+        private List<CGLSheet> _sheets = new List<CGLSheet>();
+
+        public CGLSheetFactory(CutEngine cutEngine)
+        {
+            _cutEngine = cutEngine;
+
+            Build();
+
+            return;
+        }
+
+        /// <summary>
+        /// Add sheet to the list of CGLSheet
+        /// </summary>
+        private void Build()
+        {
+            int sheetIndex, StockCount, iLayout, iSheet;
+            double width, height;
+            bool sheetActive;
+
+            // Iterate by each layout and output information about each layout,
+            // such as number and length of used stocks and part indices cut from the stocks
+            for (iLayout = 0; iLayout < _cutEngine.LayoutCount; iLayout++)
+            {
+                _cutEngine.GetLayoutInfo(iLayout, out sheetIndex, out StockCount);
+                // sheetIndex is global index of the first sheet used in the layout iLayout
+                // StockCount is quantity of sheets of the same size as sheetIndex used for this layout
+                if (StockCount > 0)
+                {
+                    // Output information about each stock, such as stock Length
+                    for (iSheet = sheetIndex; iSheet < sheetIndex + StockCount; iSheet++)
+                    {
+                        if (_cutEngine.GetStockInfo(iSheet, out width, out height, out sheetActive))
+                        {
+                            _sheets.Add(new CGLSheet
+                            {
+                                Sheet = iSheet,
+                                Width = width,
+                                Height = height
+                            });
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Return list of CGLSheet
+        /// </summary>
+        public List<CGLSheet> Get()
+        {
+            return _sheets;
+        }
+    }
+}
