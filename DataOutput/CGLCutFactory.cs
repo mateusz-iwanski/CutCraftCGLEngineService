3c5fd535414d344cdb98bc0f8968a8b7cc76abf2
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:46:30 +0200

    Add CGLCutFactory class to build CGLCut objects
    Introduce CGLCutFactory in CutCraftEngineWebSocketCGLService.DataOutput.
    - Implements ICGLDataOutputFactory<CGLCut> interface.
    - Builds a list of CGLCut objects from a CutEngine instance.
    - Imports necessary namespaces: CutGLib, NLog.Layouts, System, etc.
    - Initializes fields for CutEngine, CGLCut, CGLSheet, and CGLLayout.
    - Constructor calls Build method to populate CGLCut list.
    - Build method processes stock, adds trim and actual cuts.
    - Get method returns the list of CGLCut objects.
    - Error handling for missing sheets or layouts.

---
+++ DataOutput/CGLCutFactory.cs
@@ -0,0 +1,101 @@
+﻿using CutGLib;
+using NLog.Layouts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// Build CGLCut from CutEngine
+    /// </summary>
+    public class CGLCutFactory : ICGLDataOutputFactory<CGLCut>
+    {
+        private readonly CutEngine _cutEngine;
+
+        private List<CGLCut> _cuts = new List<CGLCut>();
+        private readonly List<CGLSheet> _sheets;
+        private readonly List<CGLLayout> _layouts;
+
+        public CGLCutFactory(CutEngine cutEngine, List<CGLSheet> sheets, List<CGLLayout> layout)
+        {
+            _cutEngine = cutEngine;
+            _sheets = sheets;
+            _layouts = layout;
+
+            Build();
+
+            return;
+        }
+
+        /// <summary>
+        /// Add cuts to the list of CGLCut
+        /// </summary>
+        private void Build()
+        {
+            int StockNo, iCut;
+            long CutsCount;
+            double Width, Height, X1 = 0, Y1 = 0, X2 = 0, Y2 = 0;
+            bool active;
+
+            // Output guilltoine cuts for each sheet
+            for (StockNo = 0; StockNo < _cutEngine.StockCount; StockNo++)
+            {
+                _cutEngine.GetStockInfo(StockNo, out Width, out Height, out active);
+
+                // only the sheet used in the layout
+                if (active)
+                {
+                    var sheet = _sheets.FirstOrDefault(s => s.Sheet == StockNo) ?? throw new Exceptions.CustomException($"Can't find Sheet index ({StockNo})");
+                    var layout = _layouts.FirstOrDefault(_layouts => _layouts.Parts.Any(p => p.Sheet == sheet)) ?? throw new Exceptions.CustomException($"Can't find in list of layouts Sheet index {sheet.Sheet}");
+
+                    // First output any trim cuts for the sheet StockNo
+                    CutsCount = _cutEngine.GetStockTrimCutCount(StockNo);
+                    for (iCut = 0; iCut < CutsCount; iCut++)
+                    {
+                        _cutEngine.GetStockTrimCut(StockNo, iCut, out X1, out Y1, out X2, out Y2);
+                        _cuts.Add(new CGLCut
+                        {
+                            Cut = iCut,
+                            X1 = X1,
+                            Y1 = Y1,
+                            X2 = X2,
+                            Y2 = Y2,
+                            Sheet = sheet,
+                            Layout = layout,
+                            Trim = true
+                        });
+                    }
+
+                    // Now output any actual cuts for the sheet StockNo
+                    CutsCount = _cutEngine.GetStockCutCount(StockNo);
+                    for (iCut = 0; iCut < CutsCount; iCut++)
+                    {
+                        _cutEngine.GetStockCut(StockNo, iCut, out X1, out Y1, out X2, out Y2);
+
+                        _cuts.Add(new CGLCut
+                        {
+                            Cut = iCut,
+                            X1 = X1,
+                            Y1 = Y1,
+                            X2 = X2,
+                            Y2 = Y2,
+                            Sheet = sheet,
+                            Layout = layout,
+                            Trim = false
+                        });
+
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Get cuts list
+        /// </summary>
+        public List<CGLCut> Get() => _cuts;
+    }
+}
