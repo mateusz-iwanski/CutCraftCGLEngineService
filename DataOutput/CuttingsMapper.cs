0b18928f1a66ebf8789a70d16b34c2f8ba410564
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-25 16:49:33 +0200

    Add CuttingsMapper class for mapping CutGlib data to Cuttings
    Introduce a new CuttingsMapper class in the CutCraftEngineWebSocketCGLService.DataOutput namespace. This class maps data from the CutGlib library to a list of DataOutput Cutting objects.
    
    - Added necessary using directives for required namespaces.
    - Implemented private fields for various factories and a list of Cutting objects.
    - Provided a constructor to initialize factories using a CutEngine instance.
    - Implemented the Map method to create and return a list of Cutting objects by iterating over layouts and gathering necessary data.

---
+++ DataOutput/CuttingsMapper.cs
@@ -0,0 +1,75 @@
+﻿using CutCraftEngineData.DataOutput;
+using CutGLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    /// <summary>
+    /// Mapping CutGlib data output to list of DataOutput Cutting
+    /// </summary>
+
+    public class CuttingsMapper
+    {
+        private readonly CutEngine _cutEngine;
+
+        private readonly CGLSheetFactory _sheetFactory;
+        private CGLCutFactory _cutFactory { get; set; }
+        private readonly CGLPartFactory _partFactory;
+        private readonly CGLLayoutFactory _layoutFactory;
+        private readonly CGLLayoutStatistics2DFactory _statistics2DFactory;
+
+        private List<Cutting> _cuttings { get; set; }
+
+
+        public CuttingsMapper(CutEngine cutEngine)
+        {
+            _cutEngine = cutEngine;
+
+            _sheetFactory = new CGLSheetFactory(_cutEngine);
+            _partFactory = new CGLPartFactory(_cutEngine, _sheetFactory.Get());
+            _layoutFactory = new CGLLayoutFactory(_cutEngine, _sheetFactory.Get(), _partFactory.Get());
+            _cutFactory = new CGLCutFactory(_cutEngine, _sheetFactory.Get(), _layoutFactory.Get());
+            _statistics2DFactory = new CGLLayoutStatistics2DFactory(_cutEngine, _layoutFactory.Get(), _sheetFactory.Get());
+        }
+
+        public List<Cutting> Map()
+        {
+            _cuttings = new List<Cutting>();
+
+            foreach (var layout in _layoutFactory.Get())
+            {
+                var a = _statistics2DFactory.Get();
+
+                // get statistcs for layout
+                var statistics2DFactory = _statistics2DFactory.Get().First(s => s.Layout.Layout == layout.Layout);  // FirstOrDefault(s => s.Layout.Layout == layout.Layout);
+                CuttingStatistics cuttingStatisticsMapper = new CuttingStatistics() { _2d = new Cutting2DStatisticsMapper(statistics2DFactory).Map() };
+
+                // get parts for layout
+                List<CuttingPiece> piecesInLayoutMapper = new PiecesMapper(layout.Parts).Map();
+
+                // get cuts for layout
+                var cutsInLayout = _cutFactory.Get().Where(c => c.Layout.Layout == layout.Layout).ToList();
+                List<Cut> cuts = new CutsMapper(cutsInLayout).Map();
+
+                _cuttings.Add(new Cutting()
+                {
+                    stockItemId = layout.Layout,
+                    quantity = layout.CountOfSheets,
+                    statistics = cuttingStatisticsMapper,
+                    pieces = piecesInLayoutMapper,
+                    rest = new List<CuttingRest>(),
+                    cuts = cuts
+                });
+            }
+
+            return _cuttings;
+        }
+
+
+
+    }
+}
