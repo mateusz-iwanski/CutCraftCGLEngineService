99cef6fe02cf0190680b25ad91f2d119e71a0d9c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-26 13:03:33 +0200

    Add CGLDefaultUnitsFactory class with Get method
    Introduced the CGLDefaultUnitsFactory class in the new namespace
    CutCraftEngineWebSocketCGLService.DataOutput. This class implements
    the ICGLDataOutputFactory<DefaultUnits> interface and includes a
    constructor and a Get method. The Get method returns a list with a
    single DefaultUnits object containing hardcoded values for length,
    field, and angle. Added necessary using directives for common
    namespaces and CutCraftEngineData.DefaultUnits.

---
+++ DataOutput/CGLDefaultUnitsFactory.cs
@@ -0,0 +1,36 @@
+﻿using CutCraftEngineData.DefaultUnits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataOutput
+{
+    public class CGLDefaultUnitsFactory : ICGLDataOutputFactory<DefaultUnits>
+    {
+        /// <summary>
+        /// Create default units
+        /// 
+        /// CutGlib always uses the same default units, so we can hardcode them.
+        /// </summary>
+        /// <return>DefaulUnits in the list. There is always only one object in the list</return>
+        public CGLDefaultUnitsFactory() 
+        {
+            return;
+        }
+
+        public List<DefaultUnits> Get() =>
+            new List<DefaultUnits>
+            {
+                { 
+                    new DefaultUnits(
+                        _length: "mm",
+                        _field: "sqmm",
+                        _angle: "deg"
+                        )
+                }
+            };
+        
+    }
+}
