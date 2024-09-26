0a766e92c8443881b3141cd5d6dc856e69944909
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-26 12:58:34 +0200

    Add CutEngineEventArgs class with properties and constructor
    Introduced a new namespace `CutCraftEngineWebSocketCGLService.CGLCalculator`.
    Created the `CutEngineEventArgs` class within this namespace.
    Added a summary documentation comment for the class.
    Defined a read-only property `CutEngine` of type `CutEngine`.
    Added a constructor to initialize the `CutEngine` property.
    Included using directives for `CutGLib` and common .NET namespaces.

---
+++ CGLCalculator/CutEngineEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using CutGLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.CGLCalculator
+{
+    /// <summary>
+    /// Custom event arguments for CutEngine event.
+    /// </summary>
+    public class CutEngineEventArgs : EventArgs
+    {
+        public CutEngine CutEngine { get; }
+
+        public CutEngineEventArgs(CutEngine cutEngine)
+        {
+            CutEngine = cutEngine;
+        }
+    }
+}
