969b6016866bdbad65618f3486286c151076655a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-26 13:02:18 +0200

    Add FileExporter class for data export functionality
    Introduce a new FileExporter class in the CutCraftEngineWebSocketCGLService.DataExporter namespace.
    - Add necessary using directives for system namespaces.
    - Include a private readonly field _dataExporter of type IDataFileExporter.
    - Provide a constructor to initialize the _dataExporter field.
    - Implement a public Export method to delegate export functionality.

---
+++ DataExporter/FileExporter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataExporter
+{
+    /// <summary>
+    /// Export data to a file
+    /// </summary>
+    public class FileExporter
+    {
+        private readonly IDataFileExporter _dataExporter;
+
+        public FileExporter(IDataFileExporter dataExporter)
+        {
+            _dataExporter = dataExporter;
+        }
+
+        public void Export()
+        {
+            _dataExporter.Export();
+        }
+    }
+}
