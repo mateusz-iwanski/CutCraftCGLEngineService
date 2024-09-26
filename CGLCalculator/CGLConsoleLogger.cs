cf74948b21ba3a2aa13d796ca3df9e04e32b2069
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-26 12:57:40 +0200

    Add CGLConsoleLogger for logging CutEngine execution results
    Introduced a new class `CGLConsoleLogger` in the `CutCraftEngineWebSocketCGLService.CGLCalculator` namespace to log results to the console when `CutEngine.Execute` is called. Imported necessary namespaces to support this functionality. The class includes a private readonly `_calculator` field and a constructor that subscribes to the `InitializeExecutedEvent` of `CGLCalculator`. The `Display` method is invoked upon event trigger, utilizing `CGLConsoleResultPrinter`.

---
+++ CGLCalculator/CGLConsoleLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.CGLCalculator
+{
+    /// <summary>
+    /// Console logger for CutGlib engine.
+    /// 
+    /// It is used to print the result to the console whenever Execute is called by CutEngine.
+    /// </summary>
+    /// <remarks>
+    /// CGLConsoleLogger has to be initialize before CGLCalculator is executed.
+    /// </remarks>
+    public class CGLConsoleLogger
+    {
+        private readonly CGLCalculator _calculator;
+
+        public CGLConsoleLogger(CGLCalculator calculator)
+        {
+            _calculator = calculator;
+            
+            // Subscribe to the CutEngineInitializerExecuted event. It's raise when CutEngine.Execute() is called.
+            _calculator.InitializeExecutedEvent += Display;
+        }
+
+        private void Display(object sender, EventArgs e)
+        {
+            new CGLConsoleResultPrinter(_calculator);
+        }
+    }
+}
