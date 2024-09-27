c5822b4b98c614e1ac5974ba6ec4e71f04e2a2cc
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-27 17:02:23 +0200

    Refactor StockPriority enum to CGLStockPriority
    Removed the `StockPriority` enum from `StockPriority.cs` and added a new `CGLStockPriority` enum in `CGLStockPriority.cs`. The new enum retains the same priority levels (`normal` and `high`) and is placed within the `CutCraftEngineWebSocketCGLService.DataInput` namespace. A summary comment was added to explain its usage in the `CutEngine.AddStock` method.

---
+++ DataInput/CGLStockPriority.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCraftEngineWebSocketCGLService.DataInput
+{
+    /// <summary>
+    /// If priority is normal then such stock will be used first before any actual stocks that have low priority. 
+    /// 
+    /// It is used in the CutEngine.AddStock method.
+    /// </summary>
+    public enum CGLStockPriority
+    {
+        normal = 0,
+        high = 1,
+    }
+}
