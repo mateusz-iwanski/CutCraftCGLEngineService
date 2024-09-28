8d1177ec46566aa9faba82afc612b364c574c63d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-28 14:31:20 +0200

    Refactor CGLConsoleLogger to CGLDisplayLogger
    Renamed `CGLConsoleLogger` to `CGLDisplayLogger` to better reflect its functionality. Updated XML documentation to describe the logger as a display logger for the CutGlib engine, capable of printing results to various outputs.
    
    Added a new private field `_printer` of type `Action<string>`. Updated the constructor to accept an additional `Action<string> print` parameter, with a null check, and assigned it to `_printer`.
    
    Modified the `Display` method to use `CGLResultPrinter` with the `_printer` action instead of `CGLConsoleResultPrinter`.

--- CGLCalculator/CGLDisplayLogger.cs
+++ CGLCalculator/CGLDisplayLogger.cs
@@ -4,24 +4,28 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CutCraftCGLEngineService.CGLCalculator.CGLResultPrinter;
 
 namespace CutCraftCGLEngineService.CGLCalculator
 {
     /// <summary>
-    /// Console logger for CutGlib engine.
+    /// Disply logger for CutGlib engine by CutGLib format.
     /// 
-    /// It is used to print the result to the console whenever Execute is called by CutEngine.
+    /// It is used to print the result to the console/file/e.g whenever Execute is called by CutEngine.
     /// </summary>
     /// <remarks>
-    /// CGLConsoleLogger has to be initialize before CGLCalculator is executed.
+    /// CGLDisplayLogger has to be initialize before CGLCalculator is executed.
     /// </remarks>
-    public class CGLConsoleLogger
+    public class CGLDisplayLogger
     {
         private readonly CGLCalculator _calculator;
+        private readonly Action<string> _printer;
 
-        public CGLConsoleLogger(CGLCalculator calculator)
+        public CGLDisplayLogger(CGLCalculator calculator, Action<string> print)
         {
             _calculator = calculator;
+
+            _printer = print ?? throw new ArgumentNullException(nameof(print)); ;
             
             // Subscribe to the CutEngineInitializerExecuted event. It's raise when CutEngine.Execute() is called.
             _calculator.InitializeExecutedEvent += Display;
@@ -29,7 +33,7 @@ namespace CutCraftCGLEngineService.CGLCalculator
 
         private void Display(object sender, EventArgs e)
         {
-            new CGLConsoleResultPrinter(_calculator);
+            new CGLResultPrinter(_calculator, _printer);
         }
     }
 }
